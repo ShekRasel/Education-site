7366ced328b314e81f368af053415df9c28335bd
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-09-21 23:46:18 +0600

    class name modify for shadow

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -23,10 +23,10 @@ function Blog() {
         <h1 className='text-3xl md:text-5xl font-semibold'>From The Blog</h1>
         <p className='mt-6 font-semibold text-gray-400 md:w-2/3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam error dicta recusandae consequuntur magnam id a. Temporibus, fugit eaque?</p>
 
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 shadow-md transition-shadow ease-in-out hover:shadow-lg mt-10'>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6  mt-10'>
             {
                 blogs.map((blog)=>(
-                    <div className=' overflow-hidden'>
+                    <div className=' overflow-hidden shadow-md hover:shadow-lg transition-shadow ease-in-out duration-300'>
                         <img src={blog.image} className='rounded-t-md transition transform hover:scale-110  overflow-hidden ease-in-out duration-300' alt="" />
                         <h1 className='text-2xl font-semibold mt-10  px-4'>{blog.header}</h1>
                         <p className='text-lg font-semibold mt-4 text-gray-500 px-4'>{blog.description}</p>
